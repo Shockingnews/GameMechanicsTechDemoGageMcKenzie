b253070d6be21ebc58c80645bcda88e8c955d170
Shockingnews <gagemckenzie70@gmail.com>
2026-03-11 11:38:59 -0300

    player COntroller

--- Assets/Scripts/CharacterControllerScript.cs
+++ Assets/Scripts/CharacterControllerScript.cs
@@ -1,12 +1,19 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.XR;
 
 public class CharacterControllerScript : MonoBehaviour
 {
+    public GameObject cam;
 
+    float gravity = -9.15f;
     public GameObject player;
     [SerializeField] Vector2 movementInput = new Vector2(0, 0);
+    [SerializeField] Vector2 rotationInput = new Vector2(0, 0);
+    static float xRotation;
+    static float yRotation;
+    Vector3 velocity;
     [SerializeField] CharacterController controller;
     float xMove;
     float yMove;
@@ -21,10 +28,12 @@ public class CharacterControllerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        velocity.y = gravity * Time.deltaTime;
 
         Vector3 movement = new Vector3(xMove, 0f, yMove);
         controller.Move(movement * Time.deltaTime * speed);
-        
+        controller.Move(velocity);
+
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -36,8 +45,30 @@ public class CharacterControllerScript : MonoBehaviour
         yMove = movementInput.y;
     }
 
-    public void OnJump()
+    public void OnJump(InputAction.CallbackContext context)
     {
+        if (controller.isGrounded)
+        {
+            if (context.performed)
+            {
+                Vector3 moveUp = transform.up * 5f;
+                controller.Move(moveUp);
+            }
+        }
+    }
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        //rotationInput = context.action.ReadValue<Vector2>();
+
+        //yRotation = rotationInput.y;
+        //xRotation = rotationInput.x;
+
+        //// gives you a limt on up and down movement
+        ////xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        //// rotates cam and player in the direction of mouse
+        ////cam.transform.rotation = Quaternion.Euler(0f, yRotation, 0f);
+        //transform.rotation = Quaternion.Euler( -rotationInput.y, rotationInput.x, 0f);
 
     }
 
